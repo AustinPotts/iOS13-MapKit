6f5e4951dd9b8614b15016fdb140335ee3169bda
AustinPotts <pottsevolvements@gmail.com>
2020-03-12 14:12:11 -0400

    Added Zoom to first Quake using Region

--- ios-guided-project-mapkit-starter-master/Quakes/Quakes/EarthquakesViewController.swift
+++ ios-guided-project-mapkit-starter-master/Quakes/Quakes/EarthquakesViewController.swift
@@ -26,6 +26,20 @@ class EarthquakesViewController: UIViewController {
             }
             guard let quakes = quakes else {return}
             print("quakes \(quakes.count)")
+            
+            DispatchQueue.main.async {
+                self.mapView.addAnnotations(quakes)
+                
+                //zoom to the first earthquake (sorted largest first)
+                
+                guard let quake = quakes.first else { return } //TODO Zoom to current user position
+                
+                let coordinateSpan = MKCoordinateSpan(latitudeDelta: 2, longitudeDelta: 2)
+                let region = MKCoordinateRegion(center: quake.coordinate, span: coordinateSpan)
+                
+                self.mapView.setRegion(region, animated: true)
+                
+            }
         }
 		
 	}
