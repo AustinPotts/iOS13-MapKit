92d3f5802c70eb0faf1411ee60b01816a65f3f19
AustinPotts <pottsevolvements@gmail.com>
2020-03-12 13:17:57 -0400

    Configured Quake Class Decoding Keys/ Containers

--- ios-guided-project-mapkit-starter-master/Quakes/Quakes/Quake.swift
+++ ios-guided-project-mapkit-starter-master/Quakes/Quakes/Quake.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 
-class Quakes: NSObject, Decodable {
+class Quake: NSObject, Decodable {
     
     //mag
     //place
@@ -19,26 +19,35 @@ class Quakes: NSObject, Decodable {
     
     
     let magnitude: Double
-//    let place: String
-//    let time: Date
-//    let latitiude: Double
-//    let longitude: Double
+    let place: String
+    let time: Date
+    let latitude: Double
+    let longitude: Double
     
     
     enum CodingKeys: String, CodingKey {
         case magnitude = "mag"
         case properties
+        case place
+        case time
+        case geometry
+        case coordinates
     }
     
     required init(from decoder: Decoder) throws {
         //Containers
         let container = try decoder.container(keyedBy: CodingKeys.self)
         let propertiesContainer = try container.nestedContainer(keyedBy: CodingKeys.self, forKey: .properties)
+        let geometry = try container.nestedContainer(keyedBy: CodingKeys.self, forKey: .geometry)
+        var coordinates = try geometry.nestedUnkeyedContainer(forKey: .coordinates)
         
         //Attributes
         self.magnitude = try propertiesContainer.decode(Double.self, forKey: .magnitude)
+        self.place = try propertiesContainer.decode(String.self, forKey: .place)
+        self.time = try propertiesContainer.decode(Date.self, forKey: .time)
         
-        
+        self.longitude = try coordinates.decode(Double.self)
+        self.latitude = try coordinates.decode(Double.self)
         
         super.init()
     }
--- ios-guided-project-mapkit-starter-master/Quakes/QuakesTests/QuakeTests.swift
+++ ios-guided-project-mapkit-starter-master/Quakes/QuakesTests/QuakeTests.swift
@@ -19,14 +19,14 @@ class QuakeTests: XCTestCase {
         let decoder = JSONDecoder()
         decoder.dateDecodingStrategy = .millisecondsSince1970
         let date = Date(timeIntervalSince1970: 1388620296020 / 1000.0)
-        let quake = try decoder.decode(Quakes.self, from: quakeData)
+        let quake = try decoder.decode(Quake.self, from: quakeData)
         // Assert(expected, actual)
         //XCTAssertEqual(1.29, quake.magnitude) // 1.29   == 1.28888889  FAILS!
         // Use accuracy for floating-point numbers
         XCTAssertEqual(1.29, quake.magnitude, accuracy: 0.001)
-//        XCTAssertEqual("10km SSW of Idyllwild, CA", quake.place)
-//        XCTAssertEqual(date, quake.time)
-//        XCTAssertEqual(-116.7776667, quake.longitude, accuracy: 0.001)
-//        XCTAssertEqual(33.663333299999998, quake.latitude, accuracy: 0.001)
+        XCTAssertEqual("10km SSW of Idyllwild, CA", quake.place)
+        XCTAssertEqual(date, quake.time)
+        XCTAssertEqual(-116.7776667, quake.longitude, accuracy: 0.001)
+        XCTAssertEqual(33.663333299999998, quake.latitude, accuracy: 0.001)
     }
 }
